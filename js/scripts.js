4a3168bdb37d50230f4ed8fb30eb653ea97990f6
Camden Swita and Michael Brooks <michael@>
2017-06-21 20:27:18 -0700

    Add additional if/else statements and alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,30 @@
 //business logic
-
 for (var currentNumber = 1; currentNumber <= userInput; currentNumber += 1) {
   if (currentNumber % 3 === 0) {
-    alert("ding");
+    alert("<p>ding</p>");
   } else if (currentNumber % 5 === 0) {
     alert("dong");
   } else if (currentNumber % 15 === 0) {
     alert("dingdong");
   } else if (currentNumber % 3 !== 0 || currentNumber % 5 !== 0 || currentNumber % 15 !== 0) {
-    alert(currentNumber)
+    alert(currentNumber);
   } else {
     alert("Enter a number dingus");
   }
-}
+};
 
 
 
 
 //user logic
 
-var userInput = parseInt(number)
+var userInput = parseInt($("input.number").val());
 
-$(document).ready(function() {
-  $("form#calculator").submit(function() {
-    event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
-    var result = add(number1, number2);
-    $("#output").text(result);
+$(document).ready(function(event) {
+
+  $("form.enterNumber").submit(function() {
+
+  $(".answer").append();
+  event.preventDefault();
   });
 });
