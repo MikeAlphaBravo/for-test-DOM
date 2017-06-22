62bec4946b9b3287ea215e531fb840cfb4df300d
Camden Swita and Michael Brooks <camden@>
2017-06-21 20:38:29 -0700

    add example of how to change array to unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,17 @@ $(document).ready(function(event) {
   event.preventDefault();
   });
 });
+
+// var names = [];
+//
+//
+// function displayUserName()
+// {
+//   var theName = document.getElementById("enter").value;
+//   if (theName == "" || theName.length == 0)
+//   {
+//      return false; //stop the function since the value is empty.
+//   }
+//   names.push(theName);
+//   document.getElementById("name").children[0].innerHTML += "<li>"+names[names.length-1]+"</li>";
+// }
